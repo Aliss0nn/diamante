18c9de8a180ef1dc3aef9057eb867eb682569818
alisson <alisson@DESKTOP-DPK4TR6>
2023-03-16 14:59:19 -0300

    Add project files.

---
+++ diamante/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace diamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" Digite um número ímpar: ");
+            int numero = int.Parse(Console.ReadLine());
+            int espaco = numero - 1;
+            int letra = 1;
+
+            if ( numero % 2 == 0)
+            {              
+                Console.WriteLine("O número não é ímpar");
+                Console.ReadLine();
+            }
+
+            else
+
+            for (int i = 0; i < numero; i++)
+            {
+
+                for (int j = 0; j < espaco; j++) 
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < letra; k++)
+                {
+                    Console.Write("X");
+                }
+               
+                Console.WriteLine();
+
+                if (i < (numero / 2))
+                {
+                    espaco--;
+                    letra += 2;
+                }
+                else
+                {
+                    espaco++;
+                    letra -= 2;
+                }
+                
+
+                Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }   }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
